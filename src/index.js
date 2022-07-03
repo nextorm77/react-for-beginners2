fea57986ef2c2e61103f434234068ec11d5f753d
nextomr77 <sonsw77@gmail.com>
2022-07-03 20:57:01 +0900

    6.0 Introducion

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  //<React.StrictMode> // 렌더링을 2번 발생시킴
+  <App />
+  //</React.StrictMode>
 );
