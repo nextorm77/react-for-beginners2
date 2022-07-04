44d8fff05f0e280b0b713957863f83233a7eac3e
nextomr77 <sonsw77@gmail.com>
2022-07-04 21:31:36 +0900

    test 7.0 To Do List part One

--- src/App.js
+++ src/App.js
@@ -1,43 +1,38 @@
 import { func } from "prop-types";
 import { useState, useEffect } from "react";
 
-// React.js 의 기본 동작 방식
-// : state를 변화시키면 컴포넌트를 재실행
-
-function Hello() {
-  // Cleanup 함수?
-  // 실제 필드에서 자주 사용하진 않음
-  // 아래는 설명 목적의 코딩
-  function byeFn() {
-    console.log("bye :(");
-  }
-  function hiFn() {
-    console.log("created :)");
-    return byeFn;
-  }
-
-  useEffect(hiFn, []);
-
-  /* 실제 코딩 방식
-  useEffect(() => {
-    console.log("created :)");
-
-    // Cleanup 함수
-    return () => console.log("detroyed :<");
-  }, []);
-  */
-
-  return <h1>Hello</h1>;
-}
-
-// {showing ? <Hello /> : null} => <Hello />를 파괴하고(숨김X) 생성하는 효과
+/*
+form 안의 button 기본 동작은 refresh(마치 submit한 것 처럼)
+기본 동작을 막기 위해 ~.preventDefault()
+*/
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+    // toDos.push; // 직접 state 접근 불가능
+    //console.log(toDos);
+  };
+  console.log(toDos);
+
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
