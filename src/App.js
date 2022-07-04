bb242059275601c9986bf58401f2168194bf8946
nextomr77 <sonsw77@gmail.com>
2022-07-04 23:03:45 +0900

    test 7.1 To Do List part Two

--- src/App.js
+++ src/App.js
@@ -6,21 +6,21 @@ form 안의 button 기본 동작은 refresh(마치 submit한 것 처럼)
 기본 동작을 막기 위해 ~.preventDefault()
 */
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
+  const [toDo, setToDo] = useState(""); // 초기값으로 state 타입 결정?
+  const [toDos, setToDos] = useState([]); // 초기값으로 state 타입 결정?
   const onChange = (event) => setToDo(event.target.value);
   const onSubmit = (event) => {
     event.preventDefault();
     if (toDo === "") {
       return;
     }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
-    // toDos.push; // 직접 state 접근 불가능
+    // toDos.push; // 직접 state 변경 불가능
+    setToDos((currentArray) => [toDo, ...currentArray]); // 함수로 state 변경
     //console.log(toDos);
+    setToDo(""); // 값으로 state 변경
   };
   console.log(toDos);
-
+  console.log(toDos.map((item, index) => <li key={index}>{item}</li>));
   return (
     <div>
       <h1>My To Dos({toDos.length})</h1>
@@ -33,6 +33,12 @@ function App() {
         />
         <button>Add To Do</button>
       </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
