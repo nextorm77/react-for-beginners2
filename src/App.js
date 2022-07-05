5a7ee2419cf7ee40dc4b83f81201795ab8917054
nextomr77 <sonsw77@gmail.com>
2022-07-05 11:54:52 +0900

    test 7.2 Coin Tracker

--- src/App.js
+++ src/App.js
@@ -1,46 +1,59 @@
-import { func } from "prop-types";
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
-/*
-form 안의 button 기본 동작은 refresh(마치 submit한 것 처럼)
-기본 동작을 막기 위해 ~.preventDefault()
-*/
 function App() {
-  const [toDo, setToDo] = useState(""); // 초기값으로 state 타입 결정?
-  const [toDos, setToDos] = useState([]); // 초기값으로 state 타입 결정?
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    // toDos.push; // 직접 state 변경 불가능
-    setToDos((currentArray) => [toDo, ...currentArray]); // 함수로 state 변경
-    //console.log(toDos);
-    setToDo(""); // 값으로 state 변경
-  };
-  console.log(toDos);
-  console.log(toDos.map((item, index) => <li key={index}>{item}</li>));
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]); // 오류 방지 차원에서 초기값 설정 필요
+  const [money, setMoney] = useState(0);
+  const onChange = (event) => setMoney(event.target.value);
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        // 성능을 위해 동시에 set~함수를 호출하면 1번의 리렌더링?
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+  //console.log("here");
+  console.log(money);
   return (
     <div>
-      <h1>My To Dos({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1>The Coins! {loading ? "" : `(${coins.length}건)`}</h1>
+      <input
+        value={money}
+        type="number"
+        placeholder="Input your money to buy coin"
+        onChange={onChange}
+      ></input>{" "}
+      USD
+      <br />
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select>
+          {coins.map((item, index) => (
+            <option key={item.id}>
+              {item.name} ({item.symbol}): {item.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
+      {loading ? (
+        ""
+      ) : (
+        <ul>
+          {coins.map((item, index) => (
+            <li key={index}>
+              {item.name} ({item.symbol}):{" "}
+              {Math.round(money / item.quotes.USD.price)} 주
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
+  // 일정 금액 입력(달러 기준)시 각 코인마다 몇 주를 살 수 있을까?
 }
 
 export default App;
